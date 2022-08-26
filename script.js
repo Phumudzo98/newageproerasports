4fe02a77ddfce3053eca393babc457ab83928580
Phumudzo98 <65679975+Phumudzo98@users.noreply.github.com>
2022-08-26 09:59:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var datum=new Date();
+
+var year=datum.getFullYear();
+
+
+
+/*if(hrs>=0)
+{
+    store='Good morning';
+}
+else if(hrs>=12)
+{
+    store='Good Day';
+}
+else if(hrs>=6)
+{
+    store='Good Evening';
+}
+else{
+    store='Welcome';
+}
+*/
+
+document.write(year);
